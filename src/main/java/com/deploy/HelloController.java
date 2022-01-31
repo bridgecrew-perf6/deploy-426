029ecb509eb1daca9dfcfc9d748d2b05d451e2c3
ydh6226 <ydh6226@naver.com>
2022-01-31 22:50:59 +0900

    클라이언트 ip 로깅

--- src/main/java/com/deploy/HelloController.java
+++ src/main/java/com/deploy/HelloController.java
@@ -1,13 +1,18 @@
 package com.deploy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        log.info("[{}] request", request.getRemoteAddr());
         return "hello";
     }
 }
