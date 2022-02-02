7fb96b0623da06cfd2271806efb9d43567369a03
ydh6226 <ydh6226@naver.com>
2022-02-02 19:45:48 +0900

    현재 프로파일을 반환하는 api 추가

--- src/main/java/com/deploy/HelloController.java
+++ src/main/java/com/deploy/HelloController.java
@@ -1,18 +1,31 @@
 package com.deploy;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final Environment environment;
 
     @GetMapping
     public String hello(HttpServletRequest request) {
         log.info("[{}] request", request.getRemoteAddr());
         return "hello";
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(environment.getActiveProfiles())
+                .findFirst()
+                .orElse("default");
     }
 }
